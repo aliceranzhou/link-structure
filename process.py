56dc5f0deebc3d412511f7772c1d65784b87f580
Alice-Z <alice.zhou@gmail.com>
2015-09-02 20:16:15 -0400

    Add labelling and threshold number of links

--- process.py
+++ process.py
@@ -6,13 +6,14 @@ import sys
 
 top_n = 20
 
-data = {}
-counts = {}
+data = []
 
 class Link:
-    def __init__(self, source, target): 
+    def __init__(self, date, source, target, count): 
+            self.date=date
             self.source=source
             self.target=target
+            self.count=count
 
 with open('raw.txt') as fp:
   for line in fp:
@@ -20,16 +21,7 @@ with open('raw.txt') as fp:
     arr = []
     for idx, val in enumerate(temp):
       arr.append(val.replace(" ", ""))
-    date = arr[0]
-    source = arr[1]
-    target = arr[2]
-    count = int(arr[3])
-    link = Link(source, target)
-
-    prev = 0
-    if link in data:
-        prev = data[link]
-    data[link] = prev + count
+    data.append(Link(arr[0], arr[1], arr[2], int(arr[3])))
 
 sys.stdout.write("source")
 sys.stdout.write(",")
@@ -37,11 +29,13 @@ sys.stdout.write("target")
 sys.stdout.write(",")
 sys.stdout.write("count\n")
 
-for link, count in data.iteritems():
+for link in data:
+  sys.stdout.write(str(link.date))
+  sys.stdout.write(",")
   sys.stdout.write(link.source)
   sys.stdout.write(",")
   sys.stdout.write(link.target)
   sys.stdout.write(",")
-  sys.stdout.write(str(data[link]))
+  sys.stdout.write(str(link.count))
   sys.stdout.write("\n")
 
